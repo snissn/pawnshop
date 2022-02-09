313e001b993b7831eacc41497cdcffdf4eee6e56
hotali7 <hotali0106@gmail.com>
2022-02-09 10:19:14 +0900

    fix: updated pawnshop nft collection and card

--- components/nft.js
+++ components/nft.js
@@ -1,20 +1,33 @@
 export default (props) => {
   const data = props.data
   return (
-<section className='is-primary mb-6'>
-<div className="tile">
-    <div className="tile p-5">
-        <article className="tile is-child notification is-info">
-          <p className="title">{data.name}</p>
-          <p className="subtitle">{data.description}</p>
-          <figure className="image ">
+    <div className="column is-4">
+      <div className="nft-card card is-clickable">
+        <div className="card-image">
+          <figure className="image is-square">
             <img src={data.image_url} />
           </figure>
-        </article>
-  </div>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <img className="is-rounded" src={data.owner?.profile_img_url} />
+              </figure>
+            </div>
+            <div className="media-content">
+              <p className="title">{data.owner?.user?.username}</p>
+            </div>
+          </div>
+
+          <div className="content">
+            <p className="title">{data.name}</p>
+            <p className="subtitle">{data.description}</p>
+          </div>
+        </div>
+      </div>
     </div>
-</section>
-)
+  )
 }
 
 
